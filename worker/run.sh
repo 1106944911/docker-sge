e891bb283b932a0042d061d8e1f2386f97ff1ae8
huangzheng <bupthz@gmail.com>
2018-07-19 20:45:59 +0800

    Update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+sh -x
+master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|grep -i $(hostname|awk -F- '{print $1}')|awk -F= '{print $2}')
+while True;then
+do
+  status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 sgeuser@$master_ip -p 30222 echo ok 2>&1)
 
-/usr/sbin/service rpcbind start
-mount -t nfs ${SGE_USER_HOME}  /home
-mount -t nfs ${SGE_WORK_HOME} /opt
+  if [[ $status == ok ]] ; then
+    echo 'connect master success'
+    break
+  elif [[ $status == "Permission denied"* ]] ; then
+     echo 'connect master refued'
+  else
+     echo 'connect master fail'
+  fi
+done
 useradd -u 10000 sgeuser
 echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|awk -F= '{print $2}')
+
 (sleep 10; sudo -u sgeuser bash -c "ssh ${master_ip} -p 30222 \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
 exec /usr/sbin/sshd -D
