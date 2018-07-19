3e59995e5daec87ce32fd0a419e6cffc12f319ed
huangzheng <bupthz@gmail.com>
2018-07-19 21:30:13 +0800

    Update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -x
+useradd -u 10000 sgeuser
+echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|grep -i $(hostname|awk -F- '{print $1}')|awk -F= '{print $2}')
 while true;then
 do
-  status=$(ssh -o BatchMode=yes -o ConnectTimeout=5 sgeuser@$master_ip -p 30222 echo ok 2>&1)
+  status=$(sudo -u sgeuser bash -c "ssh -o BatchMode=yes -o ConnectTimeout=5 sgeuser@$master_ip -p 30222 echo ok 2>&1"|tail -n 1)
 
   if [[ $status == ok ]] ; then
     echo 'connect master success'
@@ -14,8 +16,7 @@ do
      echo 'connect master fail'
   fi
 done
-useradd -u 10000 sgeuser
-echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
 
 (sleep 10; sudo -u sgeuser bash -c "ssh ${master_ip} -p 30222 \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
 exec /usr/sbin/sshd -D
