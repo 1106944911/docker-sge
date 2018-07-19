3cac8b1dede1de1e1743e2e09b20dafc27209e21
huangzheng <bupthz@gmail.com>
2018-07-19 18:13:12 +0800

    Update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 /usr/sbin/service rpcbind start
-mount -t nfs ${NFSHOME_PORT_2049_TCP_ADDR}:/ /home
-mount -t nfs ${NFSOPT_PORT_2049_TCP_ADDR}:/ /opt
+mount -t nfs ${SGE_USER_HOME}  /home
+mount -t nfs ${SGE_WORK_HOME} /opt
 useradd -u 10000 sgeuser
 echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-(sleep 10; sudo -u sgeuser bash -c "ssh ${SGEMASTER_PORT_22_TCP_ADDR} \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
+master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|awk -F= '{print $2}')
+(sleep 10; sudo -u sgeuser bash -c "ssh ${master_ip} -p 30222 \"sudo bash -c '. /etc/profile.d/sge.sh; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
 exec /usr/sbin/sshd -D
