4aebc12dbef37dbe6fe80c6e69becfdefd14a838
huangzheng <bupthz@gmail.com>
2018-07-20 15:48:31 +0800

    Update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -7,17 +7,21 @@ master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|grep -i $(hostname|awk -F
 while true
 do
   status=$(sudo -u sgeuser bash -c "ssh -o BatchMode=yes -o ConnectTimeout=5 sgeuser@$master_ip -p 30222 echo ok 2>&1"|tail -n 1)
+  echo $status
 
-  if [[ $status == ok ]];then
+  if [[ $status = *"ok"* ]];
+  then
     echo 'connect master success'
     break
-  elif [[ $status == "Permission denied"* ]];then
+  elif [[ $status = *"Permission denied"* ]];
+  then
      echo 'connect master refued'
+     sleep 1
   else
      echo 'connect master fail'
+     sleep 1
   fi
 done
-
-
+ 
 (sleep 10; sudo -u sgeuser bash -c "ssh ${master_ip} -p 30222 \"sudo bash -c 'echo $host_svc_ip $hostname;. /etc/profile.d/sge.sh; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
 exec /usr/sbin/sshd -D
