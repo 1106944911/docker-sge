045c130f4859666fb0da48819e25dc7bdf492006
huangzheng <bupthz@gmail.com>
2018-07-23 11:21:21 +0800

    Update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -4,6 +4,7 @@ useradd -u 10000 sgeuser
 echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 host_svc_ip=$(env|grep $(echo ${BATCH_JOB_ID}_${BATCH_TASKGROUP_NAME}${BATCH_TASK_INDEX}_service_host|tr 'a-z' 'A-Z'|tr '-' '_')|awk -F= '{print $2}')
 master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|grep -i $(hostname|awk -F- '{print $1}')|awk -F= '{print $2}')
+cat /opt/sge/hosts|grep $master_ip > /etc/hosts
 while true
 do
   status=$(sudo -u sgeuser bash -c "ssh -o BatchMode=yes -o ConnectTimeout=5 sgeuser@$master_ip -p 30222 echo ok 2>&1"|tail -n 1)
@@ -23,6 +24,6 @@ do
   fi
 done
 host_name=$(hostname -f)
-
+echo ${host_svc_ip}  ${host_name} >> /opt/sge/hosts
 (sleep 10; sudo -u sgeuser bash -c "ssh ${master_ip} -p 30222 \"sudo bash -c '. /etc/profile.d/sge.sh; echo ${host_svc_ip}  ${host_name}>>/etc/hosts; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
 exec /usr/sbin/sshd -D
