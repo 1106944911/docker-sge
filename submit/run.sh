dd58e6290517a6703b27edd162f034cd6daf3d0c
huangzheng <bupthz@gmail.com>
2018-07-23 19:17:51 +0800

    Create run.sh

---
+++ submit/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+useradd -u 10000 sgeuser
+echo "sgeuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+host_svc_ip=$(env|grep $(echo ${BATCH_JOB_ID}_${BATCH_TASKGROUP_NAME}${BATCH_TASK_INDEX}_service_host|tr 'a-z' 'A-Z'|tr '-' '_')|awk -F= '{print $2}')
+master_ip=$(env|grep SGE|grep MASTER|grep SERVICE_HOST|grep -i $(hostname|awk -F- '{print $1}')|awk -F= '{print $2}')
+
+while true
+do
+  status=$(sudo -u sgeuser bash -c "ssh -o BatchMode=yes -o ConnectTimeout=5 sgeuser@$master_ip -p 30222 echo ok 2>&1"|tail -n 1)
+  echo $status
+
+  if [[ $status = *"ok"* ]];
+  then
+    echo 'connect master success'
+    break
+  elif [[ $status = *"Permission denied"* ]];
+  then
+     echo 'connect master refued'
+     sleep 1
+  else
+     echo 'connect master fail'
+     sleep 1
+  fi
+done
+cat /opt/sge/hosts|grep $master_ip >> /etc/hosts
+host_name=$(hostname)
+echo ${host_svc_ip}  ${host_name} >> /opt/sge/hosts
+(sleep 1; sudo -u sgeuser bash -c "ssh ${master_ip} -p 30222 \"sudo bash -c '. /etc/profile.d/sge.sh; echo ${host_svc_ip}  ${host_name}>>/etc/hosts; qconf -ah `hostname -f`; qconf -as `hostname -f`'\""; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
+exec /usr/sbin/sshd -D   &
+sleep 5
+export SGE_ROOT=/opt/sge
+cd /opt/sge/bin/lx-amd64
+sudo su sgeuser bash -c '. /etc/profile.d/sge.sh; echo "/bin/hostname" | qsub'
