c4f37af64db13d1ec7b43d665a06f6cdd9b181cc
huangzheng <bupthz@gmail.com>
2018-08-01 09:09:31 +0800

    Update run.sh

--- worker/run.sh
+++ worker/run.sh
@@ -27,11 +27,12 @@ done
 host_name=$(hostname -f)
 svc_name=$(env|grep BATCH_CURRENT_HOST|awk -F "=" '{print $2}'|awk -F ","  '{for(i=1;i<=NF;i++){print $i}}'|awk -F ":" '{print $1}'|awk '{for(i = 1;i<=NF;i++){ print$i }}'|tr A-Z a-z)
 cp /etc/hosts /etc/hosts.bak
-sed  -i "s/$host_name/$svc_name $host_name/g" /etc/hosts.bak
+sed  -i "s/$host_name/svc_$svc_name $host_name/g" /etc/hosts.bak
 cat /opt/sge/hosts >> /etc/hosts.bak
 cat /etc/hosts.bak > /etc/hosts
-
-(sleep 10; cd /opt/sge; ./install_execd) &
+echo 'Add svc host, current hosts:'
+cat /etc/hosts
+(sleep 10; cd /opt/sge; ./inst_sge -x -auto install_sge_worker.conf -nobincheck) &
 
 sed -i 's/Port 22/Port 30222/' /etc/ssh/sshd_config
 sed -i 's/#   Port 22/Port 30222/' /etc/ssh/ssh_config
